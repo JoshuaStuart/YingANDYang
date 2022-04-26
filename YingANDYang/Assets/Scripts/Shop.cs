bc0b518b7797df9595da89d437e03668aa99fcca
JoshuaStuart <85209852+JoshuaStuart@users.noreply.github.com>
2022-04-26 12:32:57 +1000

    shop

--- YingANDYang/Assets/Scripts/Shop.cs
+++ YingANDYang/Assets/Scripts/Shop.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class Shop : MonoBehaviour
 {
     public int shopTicketValue;
+    public int shopGemValue;
     public Text ticketValue;
+    public Text gemValue;
 
     public Transform shopCanvas;
     public Transform levelCanvas;
@@ -18,12 +20,54 @@ public class Shop : MonoBehaviour
     public GameObject redAndBlue;
     public GameObject purpleAndGreen;
 
+    //see if purchased
+    private int redBluePurchased;
+    private int purpleGreenPurchased;
+
+    //set if purchased
+    public GameObject redBlueNot;
+    public GameObject purpleGreenNot;
+    public GameObject redBlueYes;
+    public GameObject purpleGreenYes;
+
 
     private void Awake()
     {
         shopCanvas.gameObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        shopGemValue = PlayerPrefs.GetInt("gemValue");
+        gemValue.text = shopGemValue.ToString();
+
         shopTicketValue = PlayerPrefs.GetInt("ticketValue");
         ticketValue.text = shopTicketValue.ToString();
+
+        redBluePurchased = PlayerPrefs.GetInt("redBlue");
+        purpleGreenPurchased = PlayerPrefs.GetInt("purpleGreen");
+
+        if (redBluePurchased == 0)
+        {
+            redBlueNot.gameObject.SetActive(true);
+            redBlueYes.gameObject.SetActive(false);
+        }
+        else
+        {
+            redBlueNot.gameObject.SetActive(false);
+            redBlueYes.gameObject.SetActive(true);
+        }
+
+        if (purpleGreenPurchased == 0)
+        {
+            purpleGreenNot.gameObject.SetActive(true);
+            purpleGreenYes.gameObject.SetActive(false);
+        }
+        else
+        {
+            purpleGreenNot.gameObject.SetActive(false);
+            purpleGreenYes.gameObject.SetActive(true);
+        }
     }
 
     public void OpenShop()
@@ -36,6 +80,24 @@ public class Shop : MonoBehaviour
     {
         shopCanvas.gameObject.SetActive(false);
         levelCanvas.gameObject.SetActive(true);
+    }
+
+    public void BuyRedBlue()
+    {
+        if(shopTicketValue >= 1000)
+        { 
+            PlayerPrefs.SetInt("redBlue", 1);
+            PlayerPrefs.SetInt("ticketValue", shopTicketValue - 1000);
+        }
+    }
+
+    public void BuyPurpleGreen()
+    {
+        if (shopTicketValue >= 3000)
+        {
+            PlayerPrefs.SetInt("purpleGreen", 1);
+            PlayerPrefs.SetInt("ticketValue", shopTicketValue - 3000);
+        }
     }
 
     public void SetBlackAndWhite()
@@ -61,4 +123,36 @@ public class Shop : MonoBehaviour
         purpleAndGreen.gameObject.SetActive(true);
     }
     
+    public void Buy1000Gems()
+    {
+        PlayerPrefs.SetInt("gemValue", shopGemValue + 1000);
+    }
+
+    public void Buy10000Gems()
+    {
+        PlayerPrefs.SetInt("gemValue", shopGemValue + 10000);
+    }
+
+    public void Buy3000Gems()
+    {
+        PlayerPrefs.SetInt("gemValue", shopGemValue + 3000);
+    }
+
+    public void Buy500Tickets()
+    {
+        if(shopGemValue >= 5000)
+        {
+            PlayerPrefs.SetInt("gemValue", shopGemValue - 5000);
+            PlayerPrefs.SetInt("ticketValue", shopTicketValue + 500);
+        }
+    }
+
+    public void Buy1000Tickets()
+    {
+        if(shopGemValue >= 7000)
+        {
+            PlayerPrefs.SetInt("gemValue", shopGemValue - 7000);
+            PlayerPrefs.SetInt("ticketValue", shopTicketValue + 1000);
+        }
+    }
 }
